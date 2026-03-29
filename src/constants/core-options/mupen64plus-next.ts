7f1cff3df3c60d426d1d7291d9bba0585660354d
jason rosenstiel <jason@randrcomputers.com>
2026-03-29 11:29:32 -0500

    Delete src directory

--- src/constants/core-options/mupen64plus-next.ts
+++
@@ -1,3 +0,0 @@
-import type { CoreOption } from './interface.ts'
-
-export const mupen64plusNextOptions: CoreOption[] = []
--- src/pages/library/navigatable-link.tsx
+++
@@ -1,5 +0,0 @@
-import { NavLink, type NavLinkProps } from 'react-router'
-
-export function NavigatableLink({ ...props }: Readonly<NavLinkProps>) {
-  return <NavLink end data-sn-enabled {...props} />
-}
